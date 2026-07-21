99edab25236dd8e2cedcd6e3f7253bf478d33ff9
Kano Jenkins <jenkins@kanohorizonia.local>
2026-07-21 19:59:23 +0000

    Publish kano-git-master-skill docs from 7504ee0705626b7b4c94f2df1263d6dc393f243c

--- api-docs/dir_44bcc8d357912c543026dbf7acbd4f18.js
+++ api-docs/dir_44bcc8d357912c543026dbf7acbd4f18.js
@@ -3,8 +3,6 @@ var dir_44bcc8d357912c543026dbf7acbd4f18 =
     [ "ai_utils.hpp", "ai__utils_8hpp_source.html", null ],
     [ "auto_model_policy.hpp", "auto__model__policy_8hpp_source.html", null ],
     [ "command_executor.hpp", "command__executor_8hpp_source.html", null ],
-    [ "command_registry.hpp", "command__registry_8hpp_source.html", null ],
     [ "command_runtime_ops.hpp", "command__runtime__ops_8hpp_source.html", null ],
-    [ "kog_config.hpp", "kog__config_8hpp_source.html", null ],
     [ "terminal_color.hpp", "terminal__color_8hpp_source.html", null ]
 ];
--- api-docs/modules_dup.js
+++ api-docs/modules_dup.js
@@ -1,6 +1,5 @@
 var modules_dup =
 [
     [ "kano.git.shell", "module_kano_8git_8shell.html", null ],
-    [ "kano.git.workspace", "module_kano_8git_8workspace.html", null ],
-    [ "kano.git.commands", "module_kano_8git_8commands.html", null ]
+    [ "kano.git.workspace", "module_kano_8git_8workspace.html", null ]
 ];
