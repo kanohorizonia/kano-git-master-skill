8a808dc19282b583938d1a0dc9fdcee208db7d72
Kano Jenkins <jenkins@kanohorizonia.local>
2026-08-13 14:46:54 +0000

    Publish kano-git-master-skill docs from a73432978faa63ae36b7dde3c060ba2eedd2ab9d

--- api-docs/dir_44bcc8d357912c543026dbf7acbd4f18.js
+++ api-docs/dir_44bcc8d357912c543026dbf7acbd4f18.js
@@ -1,6 +1,8 @@
 var dir_44bcc8d357912c543026dbf7acbd4f18 =
 [
     [ "ai_utils.hpp", "ai__utils_8hpp_source.html", null ],
+    [ "audit_run_reader.hpp", "audit__run__reader_8hpp_source.html", null ],
+    [ "audit_verification.hpp", "audit__verification_8hpp_source.html", null ],
     [ "auto_model_policy.hpp", "auto__model__policy_8hpp_source.html", null ],
     [ "command_executor.hpp", "command__executor_8hpp_source.html", null ],
     [ "command_runtime_ops.hpp", "command__runtime__ops_8hpp_source.html", null ],
--- api-docs/search/all_c.js
+++ api-docs/search/all_c.js
@@ -1,10 +1,17 @@
 var searchData=
 [
   ['operationauditcontext_0',['OperationAuditContext',['../classkano_1_1git_1_1commands_1_1OperationAuditContext.html',1,'kano::git::commands']]],
-  ['operationauditpaths_1',['OperationAuditPaths',['../structkano_1_1git_1_1commands_1_1OperationAuditPaths.html',1,'kano::git::commands']]],
-  ['operationauditspec_2',['OperationAuditSpec',['../structkano_1_1git_1_1commands_1_1OperationAuditSpec.html',1,'kano::git::commands']]],
-  ['operationcorrelationenvelope_3',['OperationCorrelationEnvelope',['../structkano_1_1git_1_1commands_1_1OperationCorrelationEnvelope.html',1,'kano::git::commands']]],
-  ['optionmetadata_4',['OptionMetadata',['../structkano_1_1git_1_1commands_1_1OptionMetadata.html',1,'kano::git::commands']]],
-  ['options_5',['options',['../structkano_1_1git_1_1commands_1_1CommandMetadata.html#a698677f23c957cf013d084e2e48e8b91',1,'kano::git::commands::CommandMetadata']]],
-  ['overview_6',['Overview',['../index.html#autotoc_md1',1,'']]]
+  ['operationauditeventpreview_1',['OperationAuditEventPreview',['../structkano_1_1git_1_1commands_1_1OperationAuditEventPreview.html',1,'kano::git::commands']]],
+  ['operationauditevidencepreview_2',['OperationAuditEvidencePreview',['../structkano_1_1git_1_1commands_1_1OperationAuditEvidencePreview.html',1,'kano::git::commands']]],
+  ['operationauditpaths_3',['OperationAuditPaths',['../structkano_1_1git_1_1commands_1_1OperationAuditPaths.html',1,'kano::git::commands']]],
+  ['operationauditrunprojection_4',['OperationAuditRunProjection',['../structkano_1_1git_1_1commands_1_1OperationAuditRunProjection.html',1,'kano::git::commands']]],
+  ['operationauditrunreadlimits_5',['OperationAuditRunReadLimits',['../structkano_1_1git_1_1commands_1_1OperationAuditRunReadLimits.html',1,'kano::git::commands']]],
+  ['operationauditrunreadpolicy_6',['OperationAuditRunReadPolicy',['../structkano_1_1git_1_1commands_1_1OperationAuditRunReadPolicy.html',1,'kano::git::commands']]],
+  ['operationauditrunreadresult_7',['OperationAuditRunReadResult',['../structkano_1_1git_1_1commands_1_1OperationAuditRunReadResult.html',1,'kano::git::commands']]],
+  ['operationauditspec_8',['OperationAuditSpec',['../structkano_1_1git_1_1commands_1_1OperationAuditSpec.html',1,'kano::git::commands']]],
+  ['operationauditverificationrequest_9',['OperationAuditVerificationRequest',['../structkano_1_1git_1_1commands_1_1OperationAuditVerificationRequest.html',1,'kano::git::commands']]],
+  ['operationcorrelationenvelope_10',['OperationCorrelationEnvelope',['../structkano_1_1git_1_1commands_1_1OperationCorrelationEnvelope.html',1,'kano::git::commands']]],
+  ['optionmetadata_11',['OptionMetadata',['../structkano_1_1git_1_1commands_1_1OptionMetadata.html',1,'kano::git::commands']]],
+  ['options_12',['options',['../structkano_1_1git_1_1commands_1_1CommandMetadata.html#a698677f23c957cf013d084e2e48e8b91',1,'kano::git::commands::CommandMetadata']]],
+  ['overview_13',['Overview',['../index.html#autotoc_md1',1,'']]]
 ];
